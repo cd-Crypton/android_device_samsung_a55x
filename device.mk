dee7bdf303f239e4cb7b46133419a0d28f521914
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-06-12 18:58:21 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,22 +14,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 AB_OTA_UPDATER := true
 TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
 
-# fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.1-impl-mock \
-    fastbootd
+android.hardware.boot@1.2-impl \ 
+android.hardware.boot@1.2-impl.recovery \ 
+android.hardware.boot@1.2-service  
 
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+AB_OTA_POSTINSTALL_CONFIG += \ 
+RUN_POSTINSTALL_system=true \ 
+POSTINSTALL_PATH_system=bin/checkpoint_gc \ 
+FILESYSTEM_TYPE_system=erofs \ 
+POSTINSTALL_OPTIONAL_system=true 
 
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
+AB_OTA_POSTINSTALL_CONFIG += \ 
+RUN_POSTINSTALL_vendor=true \ 
+POSTINSTALL_PATH_vendor=bin/checkpoint_gc \ 
+FILESYSTEM_TYPE_vendor=erofs \ 
+POSTINSTALL_OPTIONAL_vendor=true 
+
+# fastbootd PRODUCT_PACKAGES += \ 
+android.hardware.fastboot@1.1-impl-mock \ 
+fastbootd 
 
 PRODUCT_PACKAGES += \
     bootctrl.erd8845
@@ -45,14 +49,15 @@ PRODUCT_PACKAGES += \
     cppreopts.sh \
     update_engine \
     update_verifier \
-    update_engine_sideload
+    update_engine_sideload \
+    checkpoint_gc
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Shipping level
-PRODUCT_SHIPPING_API_LEVEL := 33
-PRODUCT_TARGET_VNDK_VERSION := 33
+PRODUCT_SHIPPING_API_LEVEL := 34
+PRODUCT_TARGET_VNDK_VERSION := 34
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
