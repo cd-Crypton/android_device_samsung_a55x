f1b6d0c8ecd80d68786c442a56449f6803ef7037
Carlo Dee <carlodee.official@proton.me>
2024-07-01 14:20:04 +0000

    a55x: [treewide]: Tidy-up flags and files. * Removed unnecessary flags. * Added necessary files. * Removed all flags that uses false, when its false by default if not set.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Android device tree for samsung SM-A556B (a55x)
 # Clone
-    git clone https://github.com/TheNoobDevs/android_device_samsung_a55x.git -b twrp-12.1 device/samsung/a55x
+    git clone https://github.com/MrFluffyOven/android_device_samsung_a55x.git -b twrp-12.1 device/samsung/a55x
 # Build
     export ALLOW_MISSING_DEPENDENCIES=true; . build/envsetup.sh; lunch twrp_a55x-eng; mka vendorbootimage
+
+## Credits
+- [cd-Crypton](https://github.com/cd-Crypton)
--- device.mk
+++ device.mk
@@ -8,7 +8,6 @@
 LOCAL_PATH := device/samsung/a55x
 
 # Enable virtual A/B OTA
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk) 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # The app "/system/bin/bootctl" doesn't currently exist in custom recoveries, so running it currently does nothing. So we added this.
@@ -21,38 +20,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # A/B
-AB_OTA_UPDATER := true
-TARGET_ENFORCE_AB_OTA_PARTITION_LIST := true
-
 PRODUCT_PACKAGES += \
 android.hardware.boot@1.2-impl \
 android.hardware.boot@1.2-impl.recovery \
 android.hardware.boot@1.2-service
 
 AB_OTA_POSTINSTALL_CONFIG += \
-RUN_POSTINSTALL_system=true \
-POSTINSTALL_PATH_system=bin/checkpoint_gc \
-FILESYSTEM_TYPE_system=erofs \
-POSTINSTALL_OPTIONAL_system=true
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_system=erofs \
+    POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
-RUN_POSTINSTALL_vendor=true \
-POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-FILESYSTEM_TYPE_vendor=erofs \
-POSTINSTALL_OPTIONAL_vendor=true
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=erofs \
+    POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
-android.hardware.fastboot@1.1-impl-mock \
-fastbootd 
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd 
 
 PRODUCT_PACKAGES += \
     bootctrl.erd8845
-
-#PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.erd8845 \
-    libgptutils \
-    libz \
-    libcutils
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -67,7 +57,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 32
-PRODUCT_TARGET_VNDK_VERSION := 32
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
